ae5d7caba6d841821abe10ce88a34073a70c3433
Durvankur Chavan <82404193+Durvankur4@users.noreply.github.com>
2024-07-01 19:53:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const filterButtons = document.querySelectorAll('.filter-buttons button')
 const fiterableCards = document.querySelectorAll('.fiterable-cards .card')
-const productCard = document.querySelectorAll('.card')
+
 
 
 const filterCards = e => {
@@ -20,5 +20,5 @@ const filterCards = e => {
 
 
 
-productCard.forEach(card => card.addEventListener('click', addHref))
+
 filterButtons.forEach(button => button.addEventListener("click", filterCards))
